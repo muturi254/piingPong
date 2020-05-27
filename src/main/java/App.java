6eb0bcc314c41a611da502eab0816541b0fb8559
muturi254 <petermuturi74@gmail.com>
2020-05-27 14:01:40 +0300

    initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,26 @@
+import models.PingPong;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+
+public class App {
+    public static void main(String[] args) {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("I am ping Pong program");
+        System.out.println("Enter a number");
+
+        try{
+            String stringUserNumber = bufferedReader.readLine();
+            int intUserNumber = Integer.parseInt(stringUserNumber);
+            PingPong pingPong = new PingPong();
+            ArrayList<Object> pingPongResult = pingPong.runPingPong(intUserNumber);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
