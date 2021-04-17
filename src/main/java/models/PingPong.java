2450276094748dd33a6b6a8b6267c4f88f60da2b
muturi254 <petermuturi74@gmail.com>
2021-04-18 02:20:34 +0300

    change

--- src/main/java/models/PingPong.java
+++ src/main/java/models/PingPong.java
@@ -7,6 +7,8 @@ public class PingPong {
 
     public ArrayList<Object> runPingPong(int countUpTo) {
         ArrayList<Object> result = new ArrayList<Object>();
+        System.out.println("Hello");
+
         for(int i = 1; i<= countUpTo; i++) {
             if (i%3 == 0 && i%5 == 0) {
                 result.add("pingpong");
